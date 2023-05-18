7bb1478ff451e493ef5e358d7446a07132e14b0c
DamjanVincic <damjanvincic2003@gmail.com>
2023-05-18 23:06:41 +0200

    Added a make_move function and a temporary way to play 1v1

--- main.py
+++ main.py
@@ -37,6 +37,33 @@ def get_valid_moves(board, player):
                 valid_moves.append((i, j))
     return valid_moves
 
+def make_move(board, player, move):
+    i, j = move
+    board[i][j] = player
+    opponent = WHITE if player == BLACK else BLACK
+
+    for direction in directions:
+        di, dj = direction
+        new_i, new_j = i + di, j + dj
+
+        if not (0 <= new_i <= 7 and 0 <= new_j <= 7):
+            continue
+
+        if board[new_i][new_j] == opponent:
+            positions_to_flip = []
+            while 0 <= new_i <= 7 and 0 <= new_j <= 7:
+                if board[new_i][new_j] == EMPTY:
+                    break
+                if board[new_i][new_j] == player:
+                    for position in positions_to_flip:
+                        pos_i, pos_j = position
+                        board[pos_i][pos_j] = player
+                    break
+                positions_to_flip.append((new_i, new_j))
+                new_i += di
+                new_j += dj
+    return board
+
 
 def cell_to_str(cell):
     if cell == EMPTY:
@@ -62,9 +89,55 @@ def start_game():
 
     current_player = BLACK
 
-    valid_moves = get_valid_moves(board, current_player)
+    
+    while True:
+        if current_player == BLACK:
+            valid_moves = get_valid_moves(board, current_player)
+            if len(valid_moves) == 0:
+                break
 
-    print_board(board, valid_moves)
+            print_board(board, valid_moves)
+
+            try:
+                row, col = map(int, input("Enter row and col: ").split())
+            except Exception as e:
+                print("Invalid move.")
+            
+            while (row, col) not in valid_moves:
+                print("Invalid move.")
+                try:
+                    row, col = map(int, input("Enter row and col: ").split())
+                except Exception as e:
+                    pass
+            
+            board = make_move(board, current_player, (row, col))
+            current_player = WHITE
+        elif current_player == WHITE:
+            valid_moves = get_valid_moves(board, current_player)
+            if len(valid_moves) == 0:
+                break
+
+            print_board(board, valid_moves)
+
+            try:
+                row, col = map(int, input("Enter row and col: ").split())
+            except Exception as e:
+                print("Invalid move.")
+            
+            while (row, col) not in valid_moves:
+                print("Invalid move.")
+                try:
+                    row, col = map(int, input("Enter row and col: ").split())
+                except Exception as e:
+                    pass
+            
+            board = make_move(board, current_player, (row, col))
+            current_player = BLACK
+        
+        # print_board(board, valid_moves)
+
+
+    # print_board(board, valid_moves)
 
 if __name__ == "__main__":
     start_game()
